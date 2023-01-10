d78b880ad39bf90f5793043c541dca55c7d6aa95
Justyna Duranowska <justa8866@gmail.com>
2023-01-10 09:37:14 +0100

    improve location

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-
+import { useTranslation, I18nextProvider } from 'react-i18next';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import './App.css';
+import i18n from './location/i18';
 
 function App() {
   const { t } = useTranslation();
 
   return (
-    <div className="App">
-      <h1>Start</h1>
-      <h2>Justyna</h2>
-      <header className="App-header">{t('Welcome to React')}</header>
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <div className="App">
+          <h1>Start</h1>
+          <h2>Justyna</h2>
+          <header className="App-header">{t('common:welcomeReact')}</header>
+        </div>
+      </Provider>
+    </I18nextProvider>
   );
 }
 
