e46cf14493d8fd1ec70b8d6ebff6a96802d23f95
Justyna Duranowska <justa8866@gmail.com>
2023-01-11 15:00:21 +0100

    corrections

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAppDispatch } from '../../store/hooks';
 import { signin } from '../../store/auth/authSlice';
+import RoutesList from '../../navigation/routes';
 
 function LoginPage() {
   const dispatch = useAppDispatch();
@@ -11,7 +12,7 @@ function LoginPage() {
       <button type="button" onClick={() => dispatch(signin())}>
         login
       </button>
-      <Link to="/dashboard">dashboard</Link>
+      <Link to={RoutesList.Dashboard}>dashboard</Link>
     </div>
   );
 }
