f229bb9573ad7e987b6ab8b8964e1415cef9a24d
Justyna Duranowska <justa8866@gmail.com>
2023-01-22 20:00:07 +0100

    refactor: languageSelector remove localStorage

--- src/common/LanguageSelector/LanguageSelector.tsx
+++ src/common/LanguageSelector/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
@@ -6,18 +6,11 @@ import { useTranslation } from 'react-i18next';
 import { Input, Wrapper } from './LanguageSelector.style';
 
 export default function LanguageSelect() {
-  const [t] = useTranslation();
-  const [lang, setLang] = useState(localStorage.getItem('lang') || 'pl');
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const handleChange = (event: SelectChangeEvent<string>) => {
-    setLang(event.target.value);
+    i18n.changeLanguage(event.target.value);
   };
-
-  useEffect(() => {
-    i18n.changeLanguage(lang);
-    localStorage.setItem('lang', lang);
-  }, [i18n, lang]);
 
   return (
     <Wrapper>
@@ -27,7 +20,7 @@ export default function LanguageSelect() {
           labelId="simple-select-label"
           id="simple-select"
           label={t('common.textLang')}
-          value={lang}
+          value={i18n.language}
           onChange={handleChange}
         >
           <MenuItem value="pl">PL</MenuItem>
